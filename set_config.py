49259e73c70c67a0ba7d99c4a951c0331ce75c59
RohanR-xpl <rohan.r@xploria.in>
2025-09-08 17:38:14 +0530

    logics

--- set_config.py
+++ set_config.py
@@ -6,10 +6,12 @@ print('---------------')
 print(event_path)
 print(os.path.exists(event_path))
 print('---------------')
+
 if event_path and os.path.exists(event_path):
-    event = {}
+    event = ''
     with open(event_path, 'r') as f:
-        # event = json.dumps(f)
+        event = json.load(f) 
         print(event)
-    os.environ['GITHUB_CONFIG'] = event or ''
+
+    os.environ['GITHUB_CONFIG'] = json.dumps(event) if event else ''
     print('out')
