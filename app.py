ff2e51b0f6ecf363908b6f9f62d00202f706c322
RohanR-xpl <rohan.r@xploria.in>
2025-09-08 17:46:33 +0530

    config_change

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 import os
+import json
 
-print(os.getenv('GITHUB_CONFIG'))
-
-msg = 'initial log'
-msg = 'header log'
-msg = 'Secret Key..'
-msg = 'Event Check'
-msg = 'YML check'
+GITHUB_CONFIG = json.loads(os.getenv('GITHUB_CONFIG') or "{}")
 
 
+print(GITHUB_CONFIG)
 print('Sent to Listen')
-print('Sent to Post')
-print('Changed IP')
-print('Changed Server')
 print('Used Ngrok')
-print('Added Return')
-print('Status Return')
 print('success connection')
-print('Print Check')
 
