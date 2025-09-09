74b6e2a2850db40343cb9f70e256cbc841667073
RohanR-xpl <rohan.r@xploria.in>
2025-09-09 12:38:06 +0530

    test_commands_flask

--- set_config.py
+++ set_config.py
@@ -1,1 +1,1 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://openpg:openpgpwd@http://398d0d1cb4d3.ngrok-free.app/zhap_paser_test"
+SQLALCHEMY_DATABASE_URI = "postgresql://openpg:openpgpwd@398d0d1cb4d3.ngrok-free.app/zhap_paser_test"
