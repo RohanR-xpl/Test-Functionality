b3ad66c31175e9cb5dd38bc56637ee1dc387366a
RohanR-xpl <rohan.r@xploria.in>
2025-09-09 11:44:29 +0530

    models

--- set_config.py
+++ set_config.py
@@ -1,17 +1,1 @@
-import os
-import json
-
-event_path = os.getenv('GITHUB_EVENT_PATH')
-print('---------------')
-print(event_path)
-print(os.path.exists(event_path))
-print('---------------')
-
-if event_path and os.path.exists(event_path):
-    event = ''
-    with open(event_path, 'r') as f:
-        event = json.load(f) 
-        print(event)
-
-    os.environ['GITHUB_CONFIG'] = json.dumps(event) if event else ''
-    print('out')
+SQLALCHEMY_DATABASE_URI = 'postgresql://openpg:openpgpwd@localhost/zhap_parser_test'
