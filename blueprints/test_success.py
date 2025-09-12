a7da7d1ee624c461dd3b3fbfed32a9e6b0bd9f7c
RohanR-xpl <rohan.r@xploria.in>
2025-09-12 12:12:23 +0530

    test_commands_flask

--- blueprints/test_success.py
+++ blueprints/test_success.py
@@ -2,9 +2,15 @@ from flask import Blueprint, request, jsonify
 from extensions import db
 from models.base import TestSuccess
 
+home_bp = Blueprint('home',__name__)
 test_success_bp = Blueprint("test_success", __name__, url_prefix="/test-success")
 
 @test_success_bp.route('/home')
 def printer():
     print('Print Success Route')
     return 'Hello world'
+
+@home_bp.route('/')
+def home():
+    print('Home Page')
+    return 'Home'
